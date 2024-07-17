c4931109919501789a6ad4add7639f10b829197d
James <jamesdanielxavier@gmail.com>
2024-07-17 12:29:00 -0300

    tabela medic criada

--- app/models/models.py
+++ app/models/models.py
@@ -9,3 +9,12 @@ class Patient(db.Model):
     def to_json(self):
         return {"id": self.id, "name": self.name, "age": self.age}
 
+
+class Medic(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(40), nullable=False)
+    specialty = db.Column(db.String(40), nullable=False)
+    crm = db.Column(db.String(4), nullable=False)
+
+    def to_json(self):
+        return {"name": self.name, "specialty": self.specialty, "crm": self.crm}
