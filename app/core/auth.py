f67cfe2a5c766c76a3215ff308de86da6410be8d
James <jamesdanielxavier@gmail.com>
2025-02-11 09:06:37 -0300

    callback for get_current_user(), check acess decorator in auth.py and added db to core

--- app/core/auth.py
+++ app/core/auth.py
@@ -1,21 +1,21 @@
-from app import jwt
 from functools import wraps
-from flask_jwt_extended import get_current_user, verify_jwt_in_request
+from flask_jwt_extended import get_current_user, verify_jwt_in_request, JWTManager
 from flask_jwt_extended.exceptions import NoAuthorizationError
+from app.exceptions import AuthorizationError
 from app.models.models import Medic, Patient
+
+jwt = JWTManager()
 
 
 @jwt.user_lookup_loader
 def user_lookup_callback(_jwt_header, jwt_data):
     identity = jwt_data["sub"]
 
-    medic = Medic.query.filter_by(id=identity["id"]).first()
-    if medic:
-        return medic
+    if identity["role"] == "medic":
+        return Medic.query.filter_by(id=identity["id"]).first()
 
-    patient = Patient.query.filter_by(id=identity["id"]).first()
-    if patient:
-        return patient
+    elif identity["role"] == "patient":
+        return Patient.query.filter_by(id=identity["id"]).first()
 
     return None
 
@@ -32,10 +32,10 @@ def check_access(roles=None):
             current_user = get_current_user()
 
             if current_user.role not in roles:
-                raise NoAuthorizationError("Role is now allowed")
+                raise AuthorizationError("Role is not allowed")
 
             return f(*args, **kwargs)
 
-        return decorator_function()
+        return decorator_function
 
     return decorator
